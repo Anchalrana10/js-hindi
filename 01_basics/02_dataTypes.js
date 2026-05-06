1d842f608d3e49d5daec2e255353f1d21c8a343a
Anchalrana10 <anchalrana855@gmail.com>
2026-05-06 06:01:17 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("hitesh")
+
+
+//Types of dataTypes/
+
+//=====>>>> premitive dataTypes
+
+//number => 2 to power 53
+//bigInt
+//string => ""
+//boolean => true or false
+//null => standlone value
+//undefined => value is not assigned
+//symbol => to find uniqueness 
+
+//=======>>>>>> objects
+
+console.log(typeof undefined); // => output will be undefined
+console.log(typeof null); // ==> object
