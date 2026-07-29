58aa74762006e47614f1ce0044a984c48b5dc9c2
Anchalrana10 <anchalrana855@gmail.com>
2026-07-29 06:06:27 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,12 +1,14 @@
-"use strict"; // treat all js code as newer version
 
-// alert(3+3) // we are using nodejs, not browser
+
+"use strict"; // treat all js code as newer version or newer js
+
+// alert(3+3) // we are using nodejs, not browser, in browser we can use this for alert message
 
 console.log(3
     +
-    3) // code readability should be high
+    3) // code readability should be high, dont write code like this
 
-console.log("hitesh")
+console.log("Anchal")
 
 
 //Types of dataTypes/
@@ -14,14 +16,14 @@ console.log("hitesh")
 //=====>>>> premitive dataTypes
 
 //number => 2 to power 53
-//bigInt
-//string => ""
-//boolean => true or false
-//null => standlone value
+//bigInt => number is big then use this
+//string => "" used in single and double quotes
+//boolean => true or false, user logged in or not example
+//null => standlone value ( empty value not undefined but its empty)
 //undefined => value is not assigned
-//symbol => to find uniqueness 
+//symbol => to find uniqueness {component unique}
 
 //=======>>>>>> objects
 
-console.log(typeof undefined); // => output will be undefined
+console.log(typeof undefined); // => output will be undefined { type of the value}
 console.log(typeof null); // ==> object
