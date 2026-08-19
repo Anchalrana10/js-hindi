6cd5bb8293476e3873edf572d6c59ac5d76f832a
Anchalrana10 <anchalrana855@gmail.com>
2026-08-19 04:44:45 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,16 +1,16 @@
 let score = null
 
-console.log(typeof score);
-console.log(typeof (score));
+console.log(typeof score); //type of score 
+console.log(typeof (score));// write as a method 
 
-let valueINnumber = Number(score) // score is converted to number
+let valueINnumber = Number(score) // score is converted to number , Capital based 
 console.log(typeof valueINnumber);
-console.log(valueINnumber);
+console.log(valueINnumber); //dont rely on number
 
 //conversion
 
-// 33" ==> 33
-// "33abc" ==> NaN
+// 33" ==> 33, number convert
+// "33abc" ==> NaN, not a number  , but type of NaN is number
 // true ==> 1; false ==> 0
 
 let isLoggedIn = 1
