e0d1c17ddf1e8f2fd6f8a6f481b5b87785d63206
Anchalrana10 <anchalrana855@gmail.com>
2026-08-20 05:36:03 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+//  console.log(2 > 1 );
+//  console.log(2 >= 1 );
+//  console.log(2 < 1 );
+//  console.log(2 == 1 );
+//  console.log(2 != 1 );
+
+//console.log("2" > 1);  //compare same data types
+//console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0); 
+console.log(null >= 0); // value conversion problem ,null is converted in 0
+
+console.log(undefined == 0);
+
+// we avoid these type of comparison
+
+// in js these two, one is comparison and one is equality check ,these are different things
+
+//strict check (===) values are stricky check
+console.log("2" === 2); 
