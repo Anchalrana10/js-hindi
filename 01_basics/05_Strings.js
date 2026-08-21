767df49aa8c4c65c8a7a7f0dec17d31fcf158e86
Anchalrana10 <anchalrana855@gmail.com>
2026-08-21 06:07:23 +0000

    strings

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,30 @@
+const name = "Anchal"
+const repoCOunt = 30
+
+// console.log(name + repoCOunt + "Value"); dont use this type of code nowdays
+
+//todays you can use backtick
+
+// String Interpolation
+
+// String interpolation means putting variables or expressions directly inside a string.
+
+// In JavaScript, we use template literals with backticks ` ` and ${}.
+
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCOunt}`); 
+
+
+//string declared 
+
+const gameName= new String('anchalac') //keyname value
+
+console.log(gameName[0]);
+console.log(gameName.__proto__); //these the methods
+
+
+console.log(gameName.length);
+console.log(gameName.charAt(2)); // we cant use direct t we need to add it in charcter
+
+
+
