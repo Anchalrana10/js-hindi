35b3ac944975938a7db0527032a3c877802d7885
Anchalrana10 <anchalrana855@gmail.com>
2026-08-25 06:10:37 +0000

    substring and slice

--- 01_basics/05_Strings.js
+++ 01_basics/05_Strings.js
@@ -17,7 +17,7 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCOunt}`);
 
 //string declared 
 
-const gameName= new String('anchalac') //keyname value
+const gameName= new String('anchal-ac') //keyname value
 
 console.log(gameName[0]);
 console.log(gameName.__proto__); //these the methods
@@ -25,6 +25,18 @@ console.log(gameName.__proto__); //these the methods
 
 console.log(gameName.length);
 console.log(gameName.charAt(2)); // we cant use direct t we need to add it in charcter
+console.log(gameName.indexOf('l')); // we can check that which letter is in which position by using indexof 
+
+//sometime we need to break string in some parts or santize : those methods
+
+const newString = gameName.substring(0, 4) // divided in substring
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4) // we can give -ve value to it when -ve value gives it starts reversed
+console.log(anotherString);
+
+
+// Trim and replace method
 
 
 
